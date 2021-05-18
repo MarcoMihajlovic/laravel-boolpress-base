91cd8aa94d1ddb6aa00b56065d8530e38245efa6
Marco <marcosinisa.mihajlovic@gmail.com>
2021-05-18 17:43:09 +0200

    Aggiunta dei faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(faker $faker)
     {
-        //
+        for ($i = 0; $i < 25; $i++) {
+            $new_Post = new Post();
+
+            $new_Post -> user = $faker -> name();
+            $new_Post -> title = $faker -> sentence();
+            $new_Post -> content = $faker -> text(1000);
+
+            $new_Post -> save();
+        }
     }
 }
